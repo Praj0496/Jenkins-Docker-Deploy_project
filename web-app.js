8279adb5d88518cd18c6a8d3590e0405ebdcc474
Prajwal <140489842+Praj0496@users.noreply.github.com>
2024-06-13 17:12:58 +0530

    Update web-app.js

--- web-app.js
+++ web-app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Hello, World!');
+  res.end('Hello, this message is received from docker container!');
 }
 
 const server = http.createServer(requestListener);
